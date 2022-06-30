c6d955cac3d3a9fd7991d551c0e7ddb29d11239f
sumonbarai <sumonbarai78@gmail.com>
2022-06-30 20:30:31 +0600

    bill save in database

--- src/components/shared/MyModal.js
+++ src/components/shared/MyModal.js
@@ -7,6 +7,33 @@ const MyModal = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleBilling = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    const amount = event.target.amount.value;
+    const insertedBill = {
+      name,
+      email,
+      phone,
+      amount,
+    };
+    // data post api
+    fetch("http://localhost:5000/add-billing", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(insertedBill),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
+      });
+    event.target.reset();
+    handleClose();
+  };
   return (
     <>
       <Button
@@ -22,37 +49,32 @@ const MyModal = () => {
           <Modal.Title>Add New Bill</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form className="modal-form">
+          <form className="modal-form" onSubmit={handleBilling}>
             <div>
               <label htmlFor="name">Full Name</label>
               <br />
-              <input id="name" type="text" />
+              <input id="name" type="text" name="name" />
             </div>
             <div>
-              <label htmlFor="name">Email</label>
+              <label htmlFor="email">Email</label>
               <br />
-              <input id="name" type="text" />
+              <input id="email" type="email" name="email" />
             </div>
             <div>
-              <label htmlFor="name">Phone</label>
+              <label htmlFor="phone">Phone</label>
               <br />
-              <input id="name" type="text" />
+              <input id="phone" type="text" name="phone" />
             </div>
             <div>
-              <label htmlFor="name">Paid Amount</label>
+              <label htmlFor="amount">Paid Amount</label>
               <br />
-              <input id="name" type="text" />
+              <input id="amount" type="text" name="amount" />
+            </div>
+            <div>
+              <button className="btn btn-primary mt-2">Save</button>
             </div>
           </form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
