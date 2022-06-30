e1f7b2c0fa12a823d9122fb7a5bd5fdc808218ef
sumonbarai <sumonbarai78@gmail.com>
2022-06-30 11:13:44 +0600

    header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <p>header page</p>
+    <div className="header-area">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="logo">
+            <p>Logo</p>
+          </div>
+          <div className="amount">
+            <p>
+              Paid total: <span>00</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
